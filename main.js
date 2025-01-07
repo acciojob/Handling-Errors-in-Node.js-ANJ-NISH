fb1b84b3162ad2ddba42c8edd2f5fe4659b106dc
Anurag Joshi <anuragj990@gmail.com>
2025-01-07 12:50:32 +0530

    1st try

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 const fs = require('fs');
-
-function printFileContents(filePath) {
-  
-  if(fs.existsSync(filePath))
-  {
-    fs.readFile(filePath, (err,data)=> {if(err){
-      console.log(err);
-    }else{
-      console.log(data.toString());
-    }})
-  }
-  else
-  {
-    console.log("Column '${columnName}' not found in the CSV.");
-  }
-}
+const http=require('http');
 
 const args=process.argv.slice(2);
-printFileContents(args[0]);
+
+const server=http.createServer((req,res)=>{
+  function printFileContents(filePath) {
+  
+    if(fs.existsSync(filePath))
+    {
+      fs.readFile(filePath, (err,data)=> {if(err){
+        console.log(err);
+      }else{
+        console.log(data.toString());
+      }})
+    }
+    else
+    {
+      console.log("Column '${columnName}' not found in the CSV.");
+    }
+  }
+  res.end(printFileContents(args[0]));
+})
+
+
+server.listen(3000);
